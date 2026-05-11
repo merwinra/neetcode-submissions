8536da2dc25b3bd226040f395197950914a41121
Merwin <35532152+merwinra@users.noreply.github.com>
2026-05-11 00:00:45 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 67
+float_type = 420.69
+bool_type = True
+string_type = "random string"
+list_type = [5, True, "something"]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
