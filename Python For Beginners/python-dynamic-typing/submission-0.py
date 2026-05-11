ab21eb99969e2fe5802a837af74c66c0d0fbe405
Merwin <35532152+merwinra@users.noreply.github.com>
2026-05-11 00:10:28 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 6.7
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "something"
+print(type(variable))
+variable = [420, 69, 6, 7]
+print(type(variable))
